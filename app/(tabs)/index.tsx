4b585d055e916ab0fee883deb34ee8518706540d
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-06-14 14:23:11 +0100

    feat:`Update RootLayout`

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center gap-2 text-center">
+      <Text className=" text-5xl text-black">Welcome to my app.</Text>
+
+      <Text className="text-secondary font-semibold">
+        This is a simple example of a React Native app.
+      </Text>
+      <Text className="text-secondary font-semibold">
+        It uses Tailwind CSS for styling.
+      </Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+
+const Profile = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text> Movie Profile: {id}</Text>
+    </View>
+  );
+};
+
+export default Profile;
