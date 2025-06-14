64f32cea8ec789d4e406f811b4f83a85fdde39c5
Farouk-ayo <mustaphafarouk41@gmail.com>
2025-06-14 23:19:10 +0100

    `Added expo-dev-client and updated dependencies`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,10 @@
 import { Stack } from "expo-router";
-import "./globals.css";
 import { StatusBar } from "react-native";
+import "./globals.css";
 
 export default function RootLayout() {
   return (
     <>
-      {" "}
       <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen
